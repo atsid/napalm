f72fb9f5c0168a08b7702dba1a41ae018ea430c6
tom-servo <kevin.convy@atsid.com>
2013-06-13 14:26:46 -0700

    Add build step after install by running "build" in the package.json "scripts" section. Can be used for automatic minification if the package supports it.

--- main.js
+++ main.js
@@ -9,14 +9,11 @@
 
 var express = require('express')
     , sync = require('./syncmodule')
-    , http = require('http')
     , path = require('path');
 
 var app = express();
 
-// all environments
 app.set('port', process.env.PORT || 4000);
-app.use(express.logger('dev'));
 app.get('/:lib/:version/*', sync);
 app.use(express.static(path.join(__dirname, 'serve/npm')));
 
--- syncmodule.js
+++ syncmodule.js
@@ -23,33 +23,41 @@ module.exports = function (req, res) {
         , servePath = "serve/npm/" + req.params.lib + "/" + req.params.version
         ;
     fs.stat(path, function (err, stats) {
-        if (err) {
-            process.env["HOME"] = cwd;
-            fs_extra.mkdirp(servePath, function (err) {
-                log_error(err, "At make serve path - " + servePath);
-                fs_extra.mkdirp(path, function (err) {
-                    log_error(err, "At make staging path - " + path);
-                    npm.load({cache: cwd + "/npm-cache"}, function (err) {
-                        log_error(err, "on npm load ");
-                        var tmp = npm.commands.install(path, [package], function (err) {
-                            log_error(err, "At npm install - " + path + " : " + package);
-                            if (!err) {
-                                fs_extra.copy(stagePath, servePath, function (err) {
-                                    log_error(err, "At copy - " + stagePath + " : " + servePath);
-                                    console.log("installed: " + package);
+            if (err) {
+                process.env["HOME"] = cwd;
+                fs_extra.mkdirp(servePath, function (err) {
+                    log_error(err, "At make serve path - " + servePath);
+                    fs_extra.mkdirp(path, function (err) {
+                        log_error(err, "At make staging path - " + path);
+                        npm.load({cache: cwd + "/npm-cache"}, function (err) {
+                            log_error(err, "on npm load ");
+                            npm.commands.install(path, [package], function (err) {
+                                log_error(err, "At npm install - " + path + " : " + package);
+                                if (!err) {
+                                    npm.commands.runScript([cwd + "/" + stagePath, "build"], function (err) {
+                                        log_error(err, "At npm run build script - " + stagePath);
+                                        fs_extra.copy(stagePath, servePath, function (err) {
+                                            log_error(err, "At copy - " + stagePath + " : " + servePath);
+                                            console.log("installed: " + package);
+                                            req.next();
+                                        });
+                                    });
+                                }
+                                else {
+                                    fs_extra.remove(path);
+                                    fs_extra.remove(servePath);
                                     req.next();
-                                });
-                            } else {
-                                fs_extra.remove(path);
-                                fs_extra.remove(servePath);
-                                req.next();
-                            }
+                                }
+                            });
                         });
                     });
-                })
-            });
-        } else {
-            req.next();
+                });
+            }
+            else {
+                req.next();
+            }
         }
-    });
-};
+    )
+    ;
+}
+;
